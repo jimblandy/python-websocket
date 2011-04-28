f5ffc45d51ba09185f2fe7d95e3311ff6ae04ef2
Jim Blandy <jimb@red-bean.com>
2011-04-28 12:21:36 -0700

    Adapted to version of WebSockets used by Google Chrome. Sort of.

--- websocket.py
+++ websocket.py
@@ -13,7 +13,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
 
-import sys, re, urlparse, socket, asyncore, threading
+import sys, re, urlparse, socket, asyncore, threading, random
 
 urlparse.uses_netloc.append("ws")
 urlparse.uses_fragment.append("ws")
@@ -27,9 +27,10 @@ class WebSocket(object):
         self.onmessage = kwargs.pop('onmessage', None)
         self.onerror = kwargs.pop('onerror', None)
         self.onclose = kwargs.pop('onclose', None)
+        trace = kwargs.pop('trace', False)
         if kwargs: raise ValueError('Unexpected argument(s): %s' % ', '.join(kwargs.values()))
 
-        self._dispatcher = _Dispatcher(self)
+        self._dispatcher = _Dispatcher(self, trace)
 
     def send(self, data,sync=False):
         self._dispatcher.write('\x00' + _utf8(data) + '\xff',sync)
@@ -63,17 +64,7 @@ class WebSocket(object):
         if p.query: resource += u'?' + p.query
         return (host, port, resource, secure)
 
-    #@classmethod
-    #def _generate_key(cls):
-    #    spaces = random.randint(1, 12)
-    #    number = random.randint(0, 0xffffffff/spaces)
-    #    key = list(str(number*spaces))
-    #    chars = map(unichr, range(0x21, 0x2f) + range(0x3a, 0x7e))
-    #    random_inserts = random.sample(xrange(len(key)), random.randint(1,12))
-    #    for (i, c) in [(r, random.choice(chars)) for r in random_inserts]:
-    #        key.insert(i, c)
-    #    print key
-    #    return ''.join(key)
+
 
 class WebSocketError(Exception):
     def _init_(self, value):
@@ -83,32 +74,34 @@ class WebSocketError(Exception):
         return str(self.value)
 
 class _Dispatcher(asyncore.dispatcher):
-    def __init__(self, ws):
+    def __init__(self, ws, trace=False):
+        self.trace = trace
+
         self.lock = threading.Lock() #threadsafe addon
         
         asyncore.dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connect((ws.host, ws.port))
+        self.isopen = True
         
         self.ws = ws
         self._read_buffer = ''
-        with self.lock: #threadsafe addon
-        	self._write_buffer = ''
-        self._handshake_complete = False
+        self._write_buffer = ''
         
         if self.ws.port != 80:
             hostport = '%s:%d' % (self.ws.host, self.ws.port)
         else:
             hostport = self.ws.host
         
+        self.key = _Key()
+
         fields = [
             'Upgrade: WebSocket',
             'Connection: Upgrade',
             'Host: ' + hostport,
-            'Origin: http://' + hostport,
-            #'Sec-WebSocket-Key1: %s' % WebSocket.generate_key(),
-            #'Sec-WebSocket-Key2: %s' % WebSocket.generate_key()
+            'Origin: http://' + hostport
         ]
+        fields += self.key.challenge_headers()
         if self.ws.protocol: fields['Sec-WebSocket-Protocol'] = self.ws.protocol
         if self.ws.cookie_jar:
             cookies = filter(lambda c: _cookie_for_domain(c, _eff_host(self.ws.host)) and \
@@ -118,13 +111,18 @@ class _Dispatcher(asyncore.dispatcher):
             for cookie in cookies:
                 fields.append('Cookie: %s=%s' % (cookie.name, cookie.value))
         
-        # key3 = ''.join(map(unichr, (random.randrange(256) for i in xrange(8))))
-        self.write(_utf8('GET %s HTTP/1.1\r\n' \
-                         '%s\r\n\r\n' % (self.ws.resource,
-                                         '\r\n'.join(fields))))
-                                         # key3)))
+        self.write(_utf8('GET %s HTTP/1.1\r\n%s\r\n\r\n'
+                         % (self.ws.resource,
+                            '\r\n'.join(fields)))
+                   + self.key.challenge_content())
 
-    def handl_expt(self):
+        self._current_read_handler = self._handle_header
+
+    def log(self, message):
+        if self.trace:
+            print message
+
+    def handle_expt(self):
         self.handle_error()
 
     def handle_error(self):
@@ -137,20 +135,32 @@ class _Dispatcher(asyncore.dispatcher):
 
     def handle_close(self):
         self.close()
+        self.isopen = False
         if self.ws.onclose:
             self.ws.onclose()
+
     def handle_connect (self):
         pass
         
     def handle_read(self):
-        if self._handshake_complete:
-            self._read_until('\xff', self._handle_frame)
-        else:
-            self._read_until('\r\n\r\n', self._handle_header)
+        data = self.recv(4096)
+        self.log('received: %r' % (data,))
+        self._read_buffer += data
+        while self._read_buffer:
+            handler = self._current_read_handler
+            consumed = handler(self._read_buffer)
+            if not consumed:
+                self.log('rejected: handler %s rejected %r' 
+                         % (handler.handler_name, self._read_buffer))
+                return
+            self.log('consumed: handler %s consumed %r' 
+                     % (handler.handler_name, self._read_buffer[:consumed]))
+            self._read_buffer = self._read_buffer[consumed:]
 
     def handle_write(self):
         with self.lock: #threadsafe addon
             sent = self.send(self._write_buffer)
+            self.log('sent: %r' % (self._write_buffer[:sent],))
             self._write_buffer = self._write_buffer[sent:]
 
     def writable(self):
@@ -165,37 +175,46 @@ class _Dispatcher(asyncore.dispatcher):
         if sync:
           self.handle_write()
 
-    def _read_until(self, delimiter, callback):
-        self._read_buffer += self.recv(4096)
-        pos = self._read_buffer.find(delimiter)
-        if pos >= 0:
-            pos += len(delimiter)
-        if pos > 0:
-            data = self._read_buffer[:pos]
-            self._read_buffer = self._read_buffer[pos:]
-            if data:
-                callback(data)
-
-    def _handle_frame(self, frame):
-        assert frame[-1] == '\xff'
-        if frame[0] != '\x00':
-            raise WebSocketError('WebSocket stream error')
-
-        if self.ws.onmessage:
-            self.ws.onmessage(frame[1:-1])
-        # TODO: else raise WebSocketError('No message handler defined')
-
-    def _handle_header(self, header):
-        assert header[-4:] == '\r\n\r\n'
+    def _handle_header(self, data):
+        pos = data.find('\r\n\r\n')
+        if pos < 0: return False
+        header = data[:pos]
         start_line, fields = _parse_http_header(header)
-        if start_line != 'HTTP/1.1 101 Web Socket Protocol Handshake' or \
+        if start_line != 'HTTP/1.1 101 WebSocket Protocol Handshake' or \
            fields.get('Connection', None) != 'Upgrade' or \
            fields.get('Upgrade', None) != 'WebSocket':
             raise WebSocketError('Invalid server handshake')
+        self.key.check_server_fields(fields)
+        if self.key.need_server_content:
+            self._current_read_handler = self._handle_server_digest
+        else:
+            self._current_read_handler = self._handle_frame
+        return pos + 4
 
-        self._handshake_complete = True
+    _handle_header.handler_name = 'header'
+
+    def _handle_server_digest(self, data):
+        if len(data) < 16: return False
+        self.key.check_server_body(data[:16])
         if self.ws.onopen:
             self.ws.onopen()
+        self._current_read_handler = self._handle_frame
+        return 16
+
+    _handle_server_digest.handler_name = 'server digest'
+
+    def _handle_frame(self, data):
+        pos = data.find('\xff')
+        if pos < 0: return False
+        if data[0] != '\x00':
+            raise WebSocketError('WebSocket stream error')
+        frame = data[1:pos]
+        if self.ws.onmessage:
+            self.ws.onmessage(frame)
+        # TODO: else raise WebSocketError('No message handler defined')
+        return pos + 1
+
+    _handle_frame.handler_name = 'frame'
 
 _IPV4_RE = re.compile(r'\.\d+$')
 _PATH_SEP = re.compile(r'/+')
@@ -242,3 +261,45 @@ def _cookie_for_domain(cookie, domain):
 def _utf8(s):
     return s.encode('utf-8')
 
+class _Key():
+    def __init__(self):
+        self.spaces_1 = 5 # random.randint(1, 12)
+        self.spaces_2 = 9 # random.randint(1, 12)
+        max_1 = 4294967295 / self.spaces_1
+        max_2 = 4294967295 / self.spaces_2
+        self.number_1 = 777007543 # random.randint(0, max_1)
+        self.number_2 = 114997259 # random.randint(0, max_2)
+        self.product_1 = self.number_1 * self.spaces_1
+        self.product_2 = self.number_2 * self.spaces_2
+        self.key_1 = ''.join(_Key.spaces(_Key.fluff(list(str(self.product_1))),
+                                         self.spaces_1))
+        self.key_2 = ''.join(_Key.spaces(_Key.fluff(list(str(self.product_2))),
+                                         self.spaces_2))
+        self.key_3 = ''.join((chr(random.randint(0, 255)) for i in range(8)))
+
+    def challenge_headers(self):
+        return ['Sec-WebSocket-Key1: ' + self.key_1,
+                'Sec-WebSocket-Key2: ' + self.key_2]
+    def challenge_content(self):
+        return self.key_3
+
+    def check_server_fields(self, fields): pass
+    def check_server_body(self, body): pass
+    def need_server_content(self): return True
+
+
+    mixin_chars = map(chr, range(0x21, 0x2f) + range(0x3a, 0x7e))
+
+    @classmethod
+    def fluff(cls, key):
+        for i in xrange(random.randint(1, 12)):
+            pos = random.randint(0, len(key))
+            key[pos:pos] = random.choice(_Key.mixin_chars)
+        return key
+
+    @classmethod
+    def spaces(cls, key, spaces):
+        for i in xrange(spaces):
+            pos = random.randint(1, len(key) - 1)
+            key[pos:pos] = ' '
+        return key
